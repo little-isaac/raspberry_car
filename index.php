cf2605c5304cba949e7887ac8b7e10cfdb45a307
milind <milind@Milind-pc.mshome.net>
2017-03-13 11:58:59 +0530

    Added Rotation

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 include_once 'config.php';
@@ -6,26 +6,37 @@ include_once 'config.php';
 <!DOCTYPE>
 <html>
     <head>
-        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css?t=<?php echo time(); ?>">
     </head>
     <body>
         <div id="direction"></div>
-        <?php  
-        foreach($directions as $direction=>$display){
-            ?>
-         <button class="direction" data-direction="<?php echo $display; ?>"><?php echo $direction; ?></button>
         <?php
+        foreach ($directions as $direction => $display) {
+            ?>
+            <button class="direction" data-direction="<?php echo $display; ?>"><?php echo $direction; ?></button>
+            <?php
         }
         ?>
-<!--        <button class="direction" data-direction="<?php echo $directions['FORWARD']; ?>">Forward</button>
-        <button class="direction" data-direction="<?php echo $directions['RIGHT']; ?>">Right</button>
-        <button class="direction" data-direction="<?php echo $directions['BACKWARD']; ?>">Back</button>
-        <button class="direction" data-direction="<?php echo $directions['LEFT']; ?>">Left</button>-->
+        <div class="rotation direction">
+            <div id="xRotationDiv">
+
+            </div>
+            <div id="yRotationDiv">
+
+            </div>
+            <button id="startRotation">Start Rotation</button>
+        </div>
         
+
+<!--        <button class="direction" data-direction="<?php echo $directions['FORWARD']; ?>">Forward</button>
+<button class="direction" data-direction="<?php echo $directions['RIGHT']; ?>">Right</button>
+<button class="direction" data-direction="<?php echo $directions['BACKWARD']; ?>">Back</button>
+<button class="direction" data-direction="<?php echo $directions['LEFT']; ?>">Left</button>-->
+
         <script   src="https://code.jquery.com/jquery-2.2.4.min.js"   integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="   crossorigin="anonymous"></script>
         <script>
-        window.directions = <?php echo json_encode($directions) ?>;
+            window.directions = <?php echo json_encode($directions) ?>;
         </script>
-        <script   src="js/main.js"></script>
+        <script   src="js/main.js?t=<?php echo time(); ?>"></script>
     </body>
 </html>	
--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 var $keyBtn = $(".direction");
 var $direction = $("#direction");
+var $startRotation = $("#startRotation");
+var $xRotationDiv = $("#xRotationDiv");
+var $yRotationDiv = $("#yRotationDiv");
+var rotationPath = "http://192.168.1.102:8080";
 var interface = "interface/interfaceIndex.php";
 $(document).ready(function () {
     addClickEvent();
@@ -11,15 +15,37 @@ function addClickEvent() {
         var direction = $(this).attr("data-direction");
         sendDirection(direction);
     });
-}
-function sendDirection(direction) {
-    $.post(interface, {
-        direction: direction
-    }, function (data) {
-        $direction.html(data.direction);
+    $startRotation.click(function () {
+//        getRotation();
+        setInterval(getRotation, 100);
     });
 }
-
+function getRotation() {
+    $.ajax({
+        url: rotationPath,
+        // The name of the callback parameter, as specified by the YQL service
+        jsonp: "callback",
+        // Tell jQuery we're expecting JSONP
+        dataType: "jsonp",
+        // Work with the response
+        success: function (data) {
+//            var data = $.parseJSON(dataObject);
+            window.y = data.y;
+            if(window.y > 10 || window.y < -10){
+                if(window.y > 10){
+                    sendDirection('back');
+                }
+                else{
+                    sendDirection('forward');
+                }
+            }
+            else{
+                sendDirection('stop');
+            }
+            $yRotationDiv.html("Y : "+data.y);
+        }
+    });
+}
 function addKeyPressEvent() {
     $(document).keydown(function (event) {
         switch (event.which) {
@@ -31,13 +57,20 @@ function addKeyPressEvent() {
                 break;
             case 40 : // 40 for backward
                 sendDirection(window.directions.BACKWARD);
-                break;    
+                break;
             case 37 : // 40 for left
                 sendDirection(window.directions.LEFT);
-                break;    
+                break;
             case 32 : // 40 for left
                 sendDirection(window.directions.STOP);
-                break;    
+                break;
         }
+    });
+}
+function sendDirection(direction) {
+    $.post(interface, {
+        direction: direction
+    }, function (data) {
+        $direction.html(data.direction);
     });
 }
